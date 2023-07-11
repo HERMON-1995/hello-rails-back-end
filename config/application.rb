27113a754bcb101c20ddcd3f622681158f5ed1db
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-11 19:09:04 +0300

    Allow cross-origin recourses sharing to interact the backend 3001 with the front-end 3000

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module HelloRailsBackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000' # Replace with the appropriate origin URL
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
